02dcd078f15d6dcd0033097d53a2e3e1842d1664
kosuke-17 <tamutamu17@icloud.com>
2021-12-06 14:32:02 +0900

    tailwindcss導入

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  //pagesとcomponentsで使われているutilityだけを読み込む
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
